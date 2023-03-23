53ee747788dc01846c85de6dd3489822a6ecac5b
Wwright91 <wisdomwright-martin@pursuit.org>
2023-03-22 20:41:24 -0400

    class and attributes created

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,29 @@
 // Create class below
 
+class Tamagotchi {
+    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false) {
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.mood = mood;
+        this.sick = sick;
+        this.rehomed = rehomed;
+    }
+    greet() {
+        console.log(`Hello, I'm ${this.name}!`)
+    }
+    status() {
+        console.log(`My mood is: ${this.mood} \n I am this full: ${this.full} \n My energy is: ${this.energy} \n I am ${this.sick ? "sick" : "not sick"}`)
+    }
+    eat() {
+        this.full += 2
+        this.energy -= 1
+        if (this.full > 10) {
+            this.sick = true
+        }
+    }
+}
+
+
 // Do not edit below this line
 module.exports = Tamagotchi;
