0643590af3c0a4e8aefc79a1531a3e2895fbdbd3
hector918 <hector_zhong@outlook.com>
2023-03-22 21:40:40 -0400

    oop inheritance done

--- food.js
+++ food.js
@@ -1,4 +1,28 @@
 // Create class below
-
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  prepare (){
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh (){
+    //``
+    //`ice cream has spoiled.`
+    if(this.daysToSpoil>0){
+      console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+      this.fresh = true;
+    }else{
+      console.log(`${this.name} has spoiled.`);
+      this.fresh = false;
+    }
+  }
+  aDayPasses (){
+    this.daysToSpoil--;
+    this.isFresh();
+  }
+}
 // Do not edit below this line
 module.exports = Food;
