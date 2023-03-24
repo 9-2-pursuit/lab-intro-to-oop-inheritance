f623db63d28c57372c8a069048fffbcf6752c431
i-guerrero <isaacguerrero@pursuit.org>
2023-03-23 22:49:58 -0400

    Add properties to Tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,15 @@
 // Create class below
 
-class Tamagotchi {}
+class Tamagotchi {
+  constructor(name, energy, full, mood, sick, rehomed) {
+    this.name = name;
+    this.energy = energy || 9;
+    this.full = full || 8;
+    this.mood = mood || 6;
+    this.sick = sick || false;
+    this.rehomed = rehomed || false;
+  }
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
