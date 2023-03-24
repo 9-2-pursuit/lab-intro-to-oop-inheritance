ae0045f9bb6193ac9da3c5d22859cb75468871b2
i-guerrero <isaacguerrero@pursuit.org>
2023-03-24 11:49:45 -0400

    Update medicate method on Tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -34,6 +34,7 @@ class Tamagotchi {
     if (this.sick) {
       this.full = 9;
       this.energy -= 3;
+      this.sick = false;
     } else {
       console.log(`refusal to take medicine`);
       this.energy--;
