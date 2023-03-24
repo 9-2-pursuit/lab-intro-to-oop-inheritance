b237ff053aabd23eff3d56e7d129e0e40db15ac1
i-guerrero <isaacguerrero@pursuit.org>
2023-03-23 22:58:47 -0400

    Add status method to Tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -13,7 +13,18 @@ class Tamagotchi {
   greet() {
     console.log(`Hello, I'm ${this.name}`);
   }
+
+  status() {
+    console.log(
+      `My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${
+        this.energy
+      }\nI am ${this.sick ? "" : "not "}sick`
+    );
+  }
 }
+
+const pet = new Tamagotchi("Albert", 2, 3, 5, false, true);
+pet.status();
 
 // Do not edit below this line
 module.exports = Tamagotchi;
