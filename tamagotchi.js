e67bc0b7af7c064097fc38ae1096cba211a26335
Karyn Huston <huston.karyn@gmail.com>
2023-03-26 10:42:06 -0400

    attributes for the constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -2,7 +2,11 @@
 
 class Tamagotchi {
   contructor(name, energy, full, mood, sick, rehomed) {
-    
+    this.name = name;
+    this.energy = 9;
+    this.mood = 6;
+    this.sick = false;
+    this.rehomed = false 
   }  
 }
 // Do not edit below this line
