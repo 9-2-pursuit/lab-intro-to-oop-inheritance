715a5abd961211ab133694b78aca6777a099a6f2
jasn4real <tobeobi@pursuit.org>
2023-03-29 21:58:04 -0400

    Add prepare and isFresh Method

--- food.js
+++ food.js
@@ -3,13 +3,24 @@ class Food {
     constructor(name, number, fresh = true) {
         this.name = name;
         this.daysToSpoil = number;
-        this.isFresh = fresh;
+        this.fresh = fresh;
     }
 
     prepare() {
         console.log(`${this.name} is being prepared`);
     }
 
+    isFresh(daysToSpoil) {
+        if(this.daysToSpoil < 1) {
+            console.log(`${this.name} has spoiled.`)
+            this.fresh = false;
+        } else {
+        console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        return this.fresh;
+        }
+    }
+
+    
     
 }
 // Do not edit below this line
