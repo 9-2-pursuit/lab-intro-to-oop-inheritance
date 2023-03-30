ebbdc01aea7beb68f0f704d51f34aba9f7a4d438
Brandon Bennett <brandonbennett@Pursuits-Air.lan>
2023-03-29 20:50:33 -0400

    added Food class, added prepare, isFresh, & aDayPasses methods

--- food.js
+++ food.js
@@ -1,4 +1,20 @@
 // Create class below
 
+class Food {
+    constructor(name, number, fresh = true) {
+        this.name = "Banana";
+        this.daysToSpoil = number;
+        this.fresh = true; 
+    }
+
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.fresh === true) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+    }
+}
+}
 // Do not edit below this line
 module.exports = Food;
