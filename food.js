e6d95d52dba9b456f92675266cd792ffa26d73f6
jasn4real <tobeobi@pursuit.org>
2023-03-29 21:25:14 -0400

    Food name property added

--- food.js
+++ food.js
@@ -1,4 +1,16 @@
 // Create class below
+class Food {
+    constructor(name, number, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = number;
+        this.isFresh = fresh;
+    }
 
+    prepare() {
+        console.log(`${this.name} is being prepared`);
+    }
+
+    
+}
 // Do not edit below this line
 module.exports = Food;
