f76a0994926d2d4d2478c3ca8e8f11c378d9593b
jasn4real <tobeobi@pursuit.org>
2023-03-30 21:23:49 -0400

    Add greet method

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,13 +1,29 @@
 // Create class below
 class Tamagotchi {
-    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false) {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
     this.name = name;
     this.energy = energy;
     this.full = full;
     this.mood = mood;
     this.sick = sick;
     this.rehomed = rehomed;
-    }
+  }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
+
+  
+
+
+
 }
 
 // Do not edit below this line
