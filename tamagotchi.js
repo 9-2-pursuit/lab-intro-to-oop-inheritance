aa96e499648550ebef5cb9acace44227dc977b3a
jasn4real <tobeobi@pursuit.org>
2023-03-30 21:38:37 -0400

    Add status method

--- tamagotchi.js
+++ tamagotchi.js
@@ -20,10 +20,16 @@ class Tamagotchi {
     console.log(`Hello, I'm ${this.name}!`);
   }
 
+  status() {
+    let sickMessage = sick ? "I am sick" : "I am not sick";
+    console.log(
+      `My mood is: ${this.mood} /n I am this full:${
+        this.full
+      } /n My energy is: ${this.energy} /n ${!sickMessage}`
+    );
+  }
+
   
-
-
-
 }
 
 // Do not edit below this line
