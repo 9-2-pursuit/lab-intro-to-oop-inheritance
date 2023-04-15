f6401d5cd6cc0a6fd9dace28253cd797436ee9c1
SasukeDaTony <tonytaylor@pursuit.org>
2023-04-15 12:21:59 -0400

    all tests passing

--- food.js
+++ food.js
@@ -1,4 +1,26 @@
 // Create class below
 
+
+class Food {
+    constructor(name, daysToSpoil, fresh) {
+        this.name = name || "";
+        this.daysToSpoil = daysToSpoil || NaN;
+        this.fresh = fresh || true
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh() {
+       this.daysToSpoil < 1 ? console.log(`${this.name} has spoiled.`) :
+        console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+    }
+    aDayPasses() {
+        this.daysToSpoil-= 1
+        this.isFresh(this.daysToSpoil);
+    }
+
+}
+
+
 // Do not edit below this line
 module.exports = Food;
